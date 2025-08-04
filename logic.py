ad0aaaf52a1d3cae46530feadab09872ade8cff8
NOVA <you@example.com>
2025-08-04 18:44:48 +0400

    first commit

---
+++ logic.py
@@ -0,0 +1,45 @@
+import sqlite3
+
+class DB_Manager:
+    def __init__(self, database):
+        self.database = database # имя базы данных
+        
+    def create_tables(self):
+        con = sqlite3.connect(self.database)
+        cur = con.cursor()
+        cur.execute('''
+            CREATE TABLE projects (
+                project_id INTEGER PRIMARY KEY,
+                user_id INTEGER,
+                project_name TEXT NOT NULL,
+                description TEXT,
+                url TEXT,
+                status_id INTEGER,
+                FOREIGN KEY(status_id) REFERENCES status(status_id))
+        ''')
+        con.commit()
+        con.close()
+
+
+t = DB_Manager('projects.db)
+t.create_tables()
+
+
+
+        cur.execute('''
+            CREATE TABLE status (
+                status_id INTEGER PRIMARY KEY,
+                status_name TEXT
+                )
+        ''')
+
+        con.commit()
+        con.close()
+
+
+
+
+
+
+t = DB_Manager('projects.db')
+t.create_tables()
